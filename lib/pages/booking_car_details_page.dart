913ce74516c514b3f6162048ce7f6279f5c349aa
Rinnohara9803 <sagarprajapati9803@gmail.com>
2023-01-24 21:53:34 +0545

    code refactored

--- lib/pages/booking_car_details_page.dart
+++ lib/pages/booking_car_details_page.dart
@@ -20,7 +20,7 @@ import '../widgets/review_widget.dart';
 class BookingCarDetailsPage extends StatefulWidget {
   final dynamic carId;
 
-  static String routeName = '/carDetailsPage';
+  static String routeName = '/bookingCarDetailsPage';
   const BookingCarDetailsPage({
     super.key,
     required this.carId,
@@ -31,308 +31,6 @@ class BookingCarDetailsPage extends StatefulWidget {
 }
 
 class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
-  TimeOfDay timeOfDay = TimeOfDay.now();
-  DateTime? bookFromDate;
-  String? _bookFromTime;
-  String _bookToTime = '00:00:00';
-  DateTime? bookToDate;
-  TimeOfDay? bookFromTime;
-  TimeOfDay? bookTime;
-
-  void showBookingDialog(BuildContext context, bool isMobileView, TheCar car) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return StatefulBuilder(builder: (ctx, StateSetter setState) {
-          Future<void> bookCar() async {
-            if (bookFromDate == null || bookToDate == null) {
-              FlutterToasts.showNormalToast(
-                  context, 'Provide necessary details to continue booking.');
-              return;
-            } else {
-              TheBooking booking = TheBooking(
-                bookFrom: DateFormat('yyyy-MM-dd $_bookFromTime')
-                    .format(bookFromDate!),
-                bookTo:
-                    DateFormat('yyyy-MM-dd $_bookToTime').format(bookToDate!),
-                totalCost: 0,
-              );
-
-              Provider.of<BookingsProvider>(context, listen: false)
-                  .createBooking(booking, car.id)
-                  .then((value) {
-                FlutterToasts.showNormalToast(context, 'Booking successful.');
-                Navigator.of(context).pop();
-              }).catchError((e) {
-                FlutterToasts.showErrorToast(context, e.toString());
-              });
-            }
-          }
-
-          void presentDateRangePicker() {
-            var initialDateRange = DateTimeRange(
-              start: DateTime.now(),
-              end: DateTime.now().add(
-                const Duration(hours: 24 * 3),
-              ),
-            );
-            showDateRangePicker(
-              context: context,
-              firstDate: DateTime.now(),
-              lastDate: DateTime(
-                2050,
-              ),
-              initialDateRange: initialDateRange,
-            ).then((dateRange) {
-              if (dateRange == null) {
-                return;
-              } else {
-                setState(() {
-                  initialDateRange = dateRange;
-                  bookFromDate = initialDateRange.start;
-                  bookToDate = initialDateRange.end;
-                });
-              }
-            });
-          }
-
-          void presentFromTimePicker() {
-            showTimePicker(
-              context: context,
-              initialTime: TimeOfDay.now(),
-            ).then((time) {
-              if (time == null) {
-                return;
-              } else {
-                setState(() {
-                  bookFromTime = time;
-                  String bookFromTimeString = '';
-
-                  if (bookFromTime!.hour.toString().length == 1 &&
-                      bookFromTime!.minute.toString().length == 1) {
-                    bookFromTimeString =
-                        '0${bookFromTime!.hour}:0${bookFromTime!.minute}:00';
-                  } else if (bookFromTime!.hour.toString().length == 1 &&
-                      bookFromTime!.minute.toString().length != 1) {
-                    bookFromTimeString =
-                        '0${bookFromTime!.hour}:${bookFromTime!.minute}:00';
-                  } else if (bookFromTime!.hour.toString().length != 1 &&
-                      bookFromTime!.minute.toString().length == 1) {
-                    bookFromTimeString =
-                        '${bookFromTime!.hour}:0${bookFromTime!.minute}:00';
-                  } else {
-                    bookFromTimeString =
-                        '${bookFromTime!.hour}:${bookFromTime!.minute}:00';
-                  }
-                  _bookFromTime =
-                      '${bookFromTime!.hour}:${bookFromTime!.minute}:00';
-                  _bookFromTime = bookFromTimeString;
-                });
-              }
-            });
-          }
-
-          void presentToTimePicker() {
-            showTimePicker(
-              context: context,
-              initialTime: TimeOfDay.now(),
-            ).then((time) {
-              if (time == null) {
-                return;
-              } else {
-                setState(() {
-                  bookTime = time;
-                  String bookToTimeString = '';
-
-                  if (bookTime!.hour.toString().length == 1 &&
-                      bookTime!.minute.toString().length == 1) {
-                    bookToTimeString =
-                        '0${bookTime!.hour}:0${bookTime!.minute}:00';
-                  } else if (bookTime!.hour.toString().length == 1 &&
-                      bookTime!.minute.toString().length != 1) {
-                    bookToTimeString =
-                        '0${bookTime!.hour}:${bookTime!.minute}:00';
-                  } else if (bookTime!.hour.toString().length != 1 &&
-                      bookTime!.minute.toString().length == 1) {
-                    bookToTimeString =
-                        '${bookTime!.hour}:0${bookTime!.minute}:00';
-                  } else {
-                    bookToTimeString =
-                        '${bookTime!.hour}:${bookTime!.minute}:00';
-                  }
-                  _bookToTime = bookToTimeString;
-                });
-              }
-            });
-          }
-
-          return Dialog(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 15,
-                horizontal: 15,
-              ),
-              child: SizedBox(
-                height: 210,
-                width: 210,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Continue Booking . . .',
-                      style: GoogleFonts.raleway().copyWith(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        presentDateRangePicker();
-                      },
-                      icon: const Icon(
-                        Icons.calendar_today_outlined,
-                      ),
-                      color: Colors.purple,
-                    ),
-                    bookFromWidget(presentFromTimePicker),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    bookToWidget(presentToTimePicker),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      height: 30,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.purple,
-                          ),
-                        ),
-                        onPressed: () {
-                          bookCar();
-                        },
-                        child: Text(
-                          'Book Now',
-                          style: GoogleFonts.raleway().copyWith(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        });
-      },
-    );
-  }
-
-  Row bookToWidget(void Function() presentToTimePicker) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(
-          'To        ',
-          style: GoogleFonts.raleway().copyWith(
-            color: Colors.purple,
-          ),
-        ),
-        Container(
-          height: 30,
-          padding: const EdgeInsets.all(
-            5,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.purple,
-            ),
-            borderRadius: BorderRadius.circular(
-              5,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              bookToDate == null
-                  ? 'No Date Chosen!!!'
-                  : DateFormat('yyyy-MM-dd $_bookToTime').format(bookToDate!),
-            ),
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        if (bookFromDate != null || bookToDate != null)
-          IconButton(
-            icon: const Icon(
-              Icons.watch_later_outlined,
-            ),
-            color: Colors.purple,
-            onPressed: () {
-              presentToTimePicker();
-            },
-          ),
-      ],
-    );
-  }
-
-  Row bookFromWidget(void Function() presentFromTimePicker) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(
-          'From',
-          style: GoogleFonts.raleway().copyWith(
-            color: Colors.purple,
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Container(
-          height: 30,
-          padding: const EdgeInsets.all(
-            5,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.purple,
-            ),
-            borderRadius: BorderRadius.circular(
-              5,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              bookFromDate == null
-                  ? 'No Date Chosen!!!'
-                  : DateFormat('yyyy-MM-dd $_bookFromTime')
-                      .format(bookFromDate!),
-            ),
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        if (bookFromDate != null || bookToDate != null)
-          IconButton(
-            icon: const Icon(
-              Icons.watch_later_outlined,
-            ),
-            color: Colors.purple,
-            onPressed: () {
-              presentFromTimePicker();
-            },
-          ),
-      ],
-    );
-  }
-
   Widget detailWidget(IconData iconData, String value) {
     return DottedBorder(
       borderType: BorderType.RRect,
@@ -357,30 +55,6 @@ class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
         ],
       ),
     );
-  }
-
-  @override
-  void initState() {
-    _bookFromTime = '';
-
-    bookTime = TimeOfDay.now();
-    String bookFromTimeString = '';
-
-    if (bookTime!.hour.toString().length == 1 &&
-        bookTime!.minute.toString().length == 1) {
-      bookFromTimeString = '0${bookTime!.hour}:0${bookTime!.minute}:00';
-    } else if (bookTime!.hour.toString().length == 1 &&
-        bookTime!.minute.toString().length != 1) {
-      bookFromTimeString = '0${bookTime!.hour}:${bookTime!.minute}:00';
-    } else if (bookTime!.hour.toString().length != 1 &&
-        bookTime!.minute.toString().length == 1) {
-      bookFromTimeString = '${bookTime!.hour}:0${bookTime!.minute}:00';
-    } else {
-      bookFromTimeString = '${bookTime!.hour}:${bookTime!.minute}:00';
-    }
-    _bookFromTime = bookFromTimeString;
-
-    super.initState();
   }
 
   @override
@@ -458,9 +132,12 @@ class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
                         Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            ChangeNotifierProvider<BookingResponse>.value(
-                              value: booking,
-                              child: imageWidget(car),
+                            Flexible(
+                              child:
+                                  ChangeNotifierProvider<BookingResponse>.value(
+                                value: booking,
+                                child: image(car),
+                              ),
                             ),
                             imageDetailWidget(car, isMobileView, context),
                           ],
@@ -471,6 +148,36 @@ class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
           ],
         ),
       ),
+    );
+  }
+
+  Column reviewWidget(List<Review> reviews) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 5,
+            bottom: 5,
+          ),
+          child: Text(
+            'Reviews',
+            style: GoogleFonts.raleway().copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 5,
+            bottom: 5,
+          ),
+          child: ReviewWidget(
+            reviews: reviews.take(3).toList(),
+          ),
+        ),
+      ],
     );
   }
 
@@ -577,16 +284,6 @@ class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
     );
   }
 
-  Widget imageWidget(TheCar car) {
-    final booking = Provider.of<BookingResponse>(context);
-    return Flexible(
-      child: ChangeNotifierProvider<BookingResponse>.value(
-        value: booking,
-        child: imageWidget(car),
-      ),
-    );
-  }
-
   Widget image(TheCar car) {
     final booking = Provider.of<BookingResponse>(context);
     return Stack(
@@ -595,7 +292,10 @@ class _BookingCarDetailsPageState extends State<BookingCarDetailsPage> {
           borderRadius: BorderRadius.circular(
             5,
           ),
-          child: SizedBox(
+          child: Container(
+            color: booking.status != 'active'
+                ? Colors.black12
+                : Colors.transparent,
             height: 300,
             width: double.infinity,
             child: FadeInImage(
